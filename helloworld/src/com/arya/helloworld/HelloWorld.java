5acb0b7fbca4f7753d48488b81c09e6f53a4f7fb
Sachin Pal <sachinpal9352@gmial.com>
2024-01-20 11:26:18 +0530

    Commit version 1

---
+++ helloworld/src/com/arya/helloworld/HelloWorld.java
@@ -0,0 +1,9 @@
+
+package com.arya.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+}
---
+++ helloworld/src/com/arya/helloworld/HelloWorld1.java
@@ -0,0 +1,10 @@
+package com.arya.helloworld;
+
+public class HelloWorld1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
